2ae997facc6c2a8cce82d579b7ff18d1d45f6ef3
pshassans <pramod.shashidhara@gmail.com>
2018-06-25 16:01:12 -0700

    Dep

--- clients/google_test.go
+++ clients/google_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/url"
 	"testing"
+
+	"github.com/umahmood/haversine"
 )
 
 func TestGetLatLong(t *testing.T) {
@@ -22,4 +24,17 @@ func TestGetLatLongInvalid(t *testing.T) {
 	state := "CA"
 	geoAddress := fmt.Sprintf("%s,%s,%s,%s", line1, line2, city, state)
 	GetLatLong(url.QueryEscape(geoAddress))
+}
+
+func TestGetLatLongZIPCode(t *testing.T) {
+	/*zipcode := "94086"
+	geoAddress := fmt.Sprintf("%s", zipcode)
+	res, _ := GetLatLong(url.QueryEscape(geoAddress))
+	xlog.Infof("lat :%f", res.Lat)
+	xlog.Infof("lon :%f", res.Lon)*/
+
+	oxford := haversine.Coord{Lat: 51.45, Lon: 1.15} // Oxford, UK
+	turin := haversine.Coord{Lat: 45.04, Lon: 7.42}  // Turin, Italy
+	mi, km := haversine.Distance(oxford, turin)
+	fmt.Println("Miles:", mi, "Kilometers:", km)
 }
