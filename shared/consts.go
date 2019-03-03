68817bd39aca34d86ad5f5c2559760d8208e6a8c
pramodhs <pramod.shashidhara@gmail.com>
2019-03-03 12:09:30 -0800

    Fix

--- shared/consts.go
+++ shared/consts.go
@@ -31,7 +31,7 @@ const (
 	SortByTimeLeft = "timeLeft"
 
 	// SortByDateAdded ...
-	SortByDateAdded = "dateadded"
+	SortByDateAdded = "dateAdded"
 
 	// SortByMostPopular ...
 	SortByMostPopular = "mostpopular"
